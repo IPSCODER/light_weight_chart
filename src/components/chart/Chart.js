487de6421103bb1b9e94142395c77e271bb6d006
Pravin Sawant <88920922+IPSCODER@users.noreply.github.com>
2024-03-26 11:37:36 +0530

    Update Chart.js

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,5 +1,5 @@
 import { ColorType, createChart, LineStyle } from 'lightweight-charts';
-import React, { useContext, useEffect, useRef, useState } from 'react';
+import React, { useContext, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import "./chart.css";
 import { ContextProvider } from '../../App';
 import logo from "../../logo_matigos.png"
@@ -12,6 +12,8 @@ const Chart = (props) => {
   const [selectedOption, setSelectedOption] = useState(0);
   const [data, setData] = useState([]);
   const [tooltipInfo, setTooltipInfo] = useState({ visible: false, x: 0, y: 0, content: '' });
+  const [startDate,setStartDate]= useState([])
+  const [endDate,setEndDate]= useState([])
 
   const {
     colors: {
@@ -44,7 +46,6 @@ const Chart = (props) => {
 
     chart.timeScale().fitContent();
     
-    // Add event listener to show/hide tooltip
     chart.subscribeCrosshairMove((param) => {
       if (param.time && param.point) {
         setTooltipInfo({ visible: true, x: param.point.x, y: param.point.y, content: { value: param.point.y , Time: param.time} });
@@ -58,19 +59,14 @@ const Chart = (props) => {
       return { value: a.cumsum, time: a.date };
     }));
 
-    // Highlight drawdown periods
-    // drawdownData.data.forEach(drawdown => {
-    //   const start = new Date(drawdown.Start_Date).getTime(); // Convert start date to timestamp
-    //   const end = new Date(drawdown.End_Date).getTime(); // Convert end date to timestamp
-      
-    //   // Add a check for data presence and structure
-    //   if (data.length > 0 && data[0].hasOwnProperty('cumsum')) {
-    //     const lineSeries = chart.addLineSeries({ color: 'red', lineWidth: 2, lineStyle: LineStyle.Solid }); // Adjust line style for drawdown periods
-    //     lineSeries.setData([{ time: start, value: data[0].cumsum }, { time: end, value: data[data.length - 1].cumsum }]);
-    //   } else {
-    //     console.warn('Data is empty or does not have the expected structure.');
-    //   }
-    // });
+    for (let i = 0; i < drawdownData.data.length; i++) {
+      if (data.length > 0 ) {        
+        const fillAreaSeries = chart.addAreaSeries({ topColor: 'red', bottomColor: 'transparent', lineColor: 'transparent', lineWidth: 0 });
+        fillAreaSeries.setData([{ time: startDate[i]?.date, value: startDate[i]?.value }, { time: endDate[i]?.date, value: endDate[i]?.value }]);
+      } else {
+        console.warn('Data is empty or does not have the expected structure.'); 
+      }
+    }
 
     window.addEventListener('resize', handleResize);
 
@@ -80,6 +76,32 @@ const Chart = (props) => {
     };
   }, [data, backgroundColor, lineColor, textColor, areaTopColor, areaBottomColor, selectedOption]);
 
+
+    if (startDate.length == 0 && endDate.length == 0 ) {
+      for (let i = 0; i < data?.length; i++) {
+        for (let j = 0; j < drawdownData.data?.length; j++) {
+          if (data[i]?.date === drawdownData.data[j].Start_Date) {
+            setStartDate(prevStartDate => [...prevStartDate, { date: data[i]?.date ,value: data[i]?.cumsum }]);
+          }
+        }
+      }
+
+      for (let i = 0; i < data?.length; i++) {
+        for (let j = 0; j < drawdownData.data?.length; j++) {
+          if (data[i]?.date === drawdownData.data[j].End_Date) {
+            setEndDate(prevEndDate => [...prevEndDate, { date: data[i]?.date ,value:data[i]?.cumsum }]);
+          }
+        }
+      }
+    }
+
+    useLayoutEffect(()=>{
+      setStartDate([])
+      setEndDate([])
+    },[])
+    
+
+  
   const backgroundStyle = {
     backgroundImage: `url(${backgroundImage})`, // Set background image URL
     backgroundSize: 'cover',
@@ -90,14 +112,14 @@ const Chart = (props) => {
   return (
     <div className='chart' style={backgroundStyle}>
       <div className='canvas' style={{ position: 'relative', zIndex: 1 }}>
-        <select value={selectedOption} style={{ textTransform: "uppercase" }} onChange={(e) => { setSelectedOption(e.target.value) }}>
+        <select value={selectedOption} style={{ textTransform: "uppercase" }} onChange={(e) => { setSelectedOption(e.target.value);}}>
           {props.arrayOfObjects.map((i, index) => (
             <option key={index} value={index} style={{ textTransform: "uppercase" }}>{Object.keys(i)}</option>
           ))}
         </select>
         <div ref={chartContainerRef}>
           <Tooltip {...tooltipInfo} />
-          {<img src={logo} className='png_logo' alt='logo' /> }
+          <img src={logo} className='png_logo' alt='logo' /> 
         </div>
       </div>
     </div>
